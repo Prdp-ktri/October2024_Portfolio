a9eccbae468a5e561bbc31df53f78ef802f7b417
Pradeep Khatri <pradeepkhatri44@live.com>
2025-04-24 12:48:29 +0530

    Update script.js Email js code

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ document.querySelector(".hamburger").addEventListener("click", () => {
     }, 300);
   }
 });
+
+const response = await fetch('https://api.emailjs.com/api/v1.0/email/send', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    service_id: 'your_service_id',
+    template_id: 'your_template_id',
+    user_id: 'your_user_id', // API key
+    template_params: {
+      email: email,
+      phone: phone,
+      projectInterest: projectInterest
+    }
+  })
+});
